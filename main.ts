0f6f257674af9b37a159f743c4f94134197f8e11
bnguyen86 <bnguyen@outlook.com>
2018-05-13 23:50:43 -0700

    added more steps

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-function initTooltips(selector, title) {
-    var tooltipOptions = {
-        'trigger': 'manual hover',
-        'placement': 'auto',
-        'title': title
+function initTooltips (selector : String, title : String) {
+    const tooltipOptions : Object = {
+        'trigger' : 'manual hover',
+        'placement' : 'auto',
+        'title' : title
     };
+
     $(selector).tooltip(tooltipOptions);
     $(selector).tooltip('show');
 }
+
+
 // $(() => {
 //     initTooltips('#navbarDropdownMenuLink', 'Return to the "Onboarding Badge" anytime here');
 //     initTooltips('#take-tour', 'Skip to any step in the current journey from here.');
